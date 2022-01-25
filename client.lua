f1bbd808420be080e8a6421b22ea25b1734e16a3
PakiSYT <62026251+PakiSYT@users.noreply.github.com>
2022-01-25 18:54:25 +0100

    Update client.lua

--- client.lua
+++ client.lua
@@ -8,18 +8,3 @@ Citizen.CreateThread(function()
         Citizen.Wait(60000)
     end
 end)
-
--- Napis na górze ekranu, jesli nie chcesz to wywal ;d            
-Citizen.CreateThread(function()
-    while true do
-        Citizen.Wait(5)
-        SetTextFont(0)
-        SetTextScale(0.4, 0.4)
-        SetTextColour(255, 0, 75, 255)
-        SetTextEntry("STRING")
-        AddTextComponentString("Twoja Nazwa Serwera") -- Edytujesz to jesli chcesz to miec
-        DrawText( 0.055, 0.001)       
-        DrawRect(150, 100, 3.2, 0.075, 24, 36, 46, 245)
-    end
-end)
--- pozdro
